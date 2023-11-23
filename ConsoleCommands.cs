be1ced514c693a8bd423b6e779b3cb00d8c3f45e
DEAFPS <43534349+DEAFPS@users.noreply.github.com>
2023-11-23 16:21:22 +0100

    fixed tiny oopsie

--- ConsoleCommands.cs
+++ ConsoleCommands.cs
@@ -252,7 +252,7 @@ namespace MatchZy
         }
 
         [ConsoleCommand("css_rmap", "Reloads the current map")]
-        private void OnMapReloadCommand(CCSPlayerController? player) {
+        private void OnMapReloadCommand(CCSPlayerController? player, CommandInfo command) {
             if (player == null) return;
             if (!IsPlayerAdmin(player)) {
                 SendPlayerNotAdminMessage(player);
