68a88d44f00c5b242ff872b46c27e1ca319bb109
Brendon Taylor <team@lanslide.com.au>
2024-03-11 18:09:33 +1100

    Restrict scrim teams if match is live

--- MatchZy.cs
+++ MatchZy.cs
@@ -228,8 +228,11 @@ namespace MatchZy
                 if (isMatchSetup && player != null && player.IsValid) {
                     if (int.TryParse(info.ArgByIndex(1), out int joiningTeam)) {
                         int playerTeam = (int)GetPlayerTeam(player);
-                        if (!enableMatchScrim && joiningTeam != playerTeam) {
-                            return HookResult.Stop;
+                        // if scrim, then only check once match has started
+                        if ((enableMatchScrim && matchStarted) || !enableMatchScrim) {
+                            if (joiningTeam != playerTeam) {
+                                return HookResult.Stop;
+                            }
                         }
                     }
                 }
