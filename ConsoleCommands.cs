005bc3db1a042e94cc792518e0e54853ad4de1a6
Brendon Taylor <team@lanslide.com.au>
2024-03-12 22:07:25 +1100

    Small Typo

--- ConsoleCommands.cs
+++ ConsoleCommands.cs
@@ -231,9 +231,9 @@ namespace MatchZy
             if (IsPlayerAdmin(player, "css_skipveto", "@css/config")) {
                 if (matchStarted) {
                     if (player == null) {
-                        ReplyToUserCommand(player, $"Skip veto command cannot be used if match Match has already started!");
+                        ReplyToUserCommand(player, $"Skip veto command cannot be used if match has already started!");
                     } else {
-                        player.PrintToChat($"{chatPrefix} Skip veto command cannot be used if match Match has already started!");
+                        player.PrintToChat($"{chatPrefix} Skip veto command cannot be used if match has already started!");
                     }
                 }
                 else {
