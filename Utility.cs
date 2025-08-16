7da5305ffd62e53774d2942a7000f4322162ec3d
Guilherme Carnietto Depizol <24928611+guiiicd@users.noreply.github.com>
2025-08-16 13:30:27 -0400

    Remoção da demo automatica

--- Utility.cs
+++ Utility.cs
@@ -315,7 +315,7 @@ namespace MatchZy
         private void StartLive()
         {
             SetupLiveFlagsAndCfg();
-            StartDemoRecording();
+            // StartDemoRecording();
 
             // Storing 0-0 score backup file as lastBackupFileName, so that .stop functions properly in first round.
             lastBackupFileName = $"matchzy_{liveMatchId}_{matchConfig.CurrentMapNumber}_round00.txt";
@@ -377,7 +377,7 @@ namespace MatchZy
                 // We stop demo recording if a live match was restarted
                 if (matchStarted && isDemoRecording)
                 {
-                    Server.ExecuteCommand($"tv_stoprecord");
+                    // Server.ExecuteCommand($"tv_stoprecord");
                     isDemoRecording = false;
                 }
                 // Reset match data
@@ -861,7 +861,7 @@ namespace MatchZy
             int currentMapNumber = matchConfig.CurrentMapNumber;
             Log($"[HandleMatchEnd] MAP ENDED, isMatchSetup: {isMatchSetup} matchid: {liveMatchId} currentMapNumber: {currentMapNumber} tvFlushDelay: {tvFlushDelay}");
 
-            StopDemoRecording(tvFlushDelay - 0.5f, activeDemoFile, liveMatchId, currentMapNumber);
+            // StopDemoRecording(tvFlushDelay - 0.5f, activeDemoFile, liveMatchId, currentMapNumber);
 
             string winnerName = GetMatchWinnerName();
             (int t1score, int t2score) = GetTeamsScore();
