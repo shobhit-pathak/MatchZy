acf8ac5398474bc9d2f9665de06bd46dc5ae30c1
Shobhit Pathak <pathakshobhit27@gmail.com>
2025-10-21 11:57:37 +0530

    0.8.14: noclip fix and css bump

--- MatchZy.cs
+++ MatchZy.cs
@@ -14,7 +14,7 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "0.8.13";
+        public override string ModuleVersion => "0.8.14";
 
         public override string ModuleAuthor => "WD- (https://github.com/shobhit-pathak/)";
 
